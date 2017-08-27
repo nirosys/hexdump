6909cbbda1a5f253df20217e2494bbf650d3d822
Richard Giliam <nirosys@gmail.com>
2017-08-26 19:29:33 -0500

    Add some documentation

--- hexdump.go
+++ hexdump.go
@@ -6,16 +6,27 @@ import (
 )
 
 const (
-	columns        int = 80
-	bytesPerRow    int = (columns - 1) / 4
+	// Default number of columns. Currently not configurable.
+	columns int = 80
+
+	// Number of bytes from the slice that will fit on a single line.
+	bytesPerRow int = (columns - 1) / 4
+
+	// The offset into the line buffer where the ASCII representation starts.
 	asciiStartChar int = bytesPerRow*3 + 1
 )
 
+// Array of hex characters for byte to string conversion.
 var hex []byte = []byte{
 	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
 	'A', 'B', 'C', 'D', 'E', 'F',
 }
 
+// Converts a byte slice into a single line string of space separated
+// hexadecimal values for each entry in the byte slice.
+// For example:
+//    s := hexdump.ByteToHexString([]byte{0x41, 0x42})
+//    // s == "0x41 0x42"
 func ByteToHexString(b []byte) string {
 	var buffer bytes.Buffer
 
@@ -28,6 +39,16 @@ func ByteToHexString(b []byte) string {
 	return buffer.String()
 }
 
+// Returns a string representation of the provided by slice in the typical
+// canonical Hex+ASCII format. The string is formatted into an 80 column
+// output, splitting the slice into as many bytes (hex + ascii) as can fit
+// on a single 80 column line.
+//
+// Non-printable ASCII values are represented with '.' characters.
+//
+// For example:
+//    s := hexdump.ByteToHexAsciiString([]byte{0x41, 0x42, 0x43})
+//    // s == "41 42 43  ABC"
 func ByteToHexAsciiString(b []byte) string {
 	var entire bytes.Buffer
 	var line []byte = make([]byte, columns)
