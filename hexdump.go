cb5c343e5bb520174f55de0fa2cac443b23f3ada
Richard Giliam <nirosys@gmail.com>
2017-08-26 19:11:44 -0500

    Update ByteToHexAsciiString to use fixed slice for line, instead juggling buffers

--- hexdump.go
+++ hexdump.go
@@ -5,6 +5,17 @@ import (
 	"fmt"
 )
 
+const (
+	columns        int = 80
+	bytesPerRow    int = (columns - 1) / 4
+	asciiStartChar int = bytesPerRow*3 + 1
+)
+
+var hex []byte = []byte{
+	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+	'A', 'B', 'C', 'D', 'E', 'F',
+}
+
 func ByteToHexString(b []byte) string {
 	var buffer bytes.Buffer
 
@@ -18,41 +29,37 @@ func ByteToHexString(b []byte) string {
 }
 
 func ByteToHexAsciiString(b []byte) string {
-	var hex bytes.Buffer
-	var ascii bytes.Buffer
-	var buffer bytes.Buffer
-	var columns int = 80
+	var entire bytes.Buffer
+	var line []byte = make([]byte, columns)
+
+	for i := 0; i < columns; i++ {
+		line[i] = 0x20
+	}
 
 	for i := 0; i < len(b); i++ {
-		hex.WriteString(fmt.Sprintf("%02x ", b[i]))
+		lineByte := i % bytesPerRow
+		line[lineByte*3] = hex[b[i]>>4]
+		line[(lineByte*3)+1] = hex[b[i]&0x0F]
+
+		line[asciiStartChar+lineByte] = '.'
 		if b[i] > 33 && b[i] < 127 {
-			ascii.WriteString(fmt.Sprintf("%c", b[i]))
-		} else {
-			ascii.WriteByte(0x2E)
+			line[asciiStartChar+lineByte] = b[i]
 		}
-		if columns-(hex.Len()+ascii.Len()+2) < 3 {
-			buffer.Write(hex.Bytes())
-			buffer.WriteString("  ")
-			buffer.Write(ascii.Bytes())
-			buffer.WriteString("\n")
-			hex.Reset()
-			ascii.Reset()
+
+		if lineByte == bytesPerRow-1 {
+			entire.Write(line[:asciiStartChar+bytesPerRow])
+			entire.WriteString("\n")
 		}
 	}
 
-	if hex.Len() > 0 {
-		buffer.Write(hex.Bytes())
-		blank := []byte{0x20, 0x20, 0x20}
-		moar := (((columns-1)/4)*3 - hex.Len()) / 3
-		for i := 0; i < moar; i++ {
-			buffer.Write(blank)
+	if r := len(b) % bytesPerRow; r > 0 {
+		for i := r; i < bytesPerRow; i++ {
+			line[i*3] = 0x20
+			line[i*3+1] = 0x20
+			line[asciiStartChar+i] = 0x20
 		}
-
-		buffer.WriteString("  ")
-		buffer.Write(ascii.Bytes())
-		buffer.WriteString("\n")
+		entire.Write(line[:asciiStartChar+r])
 	}
-	buffer.Truncate(buffer.Len() - 1)
 
-	return buffer.String()
+	return entire.String()
 }
